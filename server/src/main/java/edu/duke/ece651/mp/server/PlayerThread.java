37ae552f641d74b8d40ef0d6612d89572e80ea11
yy295 <yy295@duke.edu>
2022-03-15 15:40:52 -0400

    refactorCode

--- server/src/main/java/edu/duke/ece651/mp/server/PlayerThread.java
+++ server/src/main/java/edu/duke/ece651/mp/server/PlayerThread.java
@@ -11,37 +11,23 @@ import edu.duke.ece651.mp.common.TurnList;
 import edu.duke.ece651.mp.server.MasterServer;
 
 public class PlayerThread<T> implements Runnable {
-  // public Socket player_socket = null;
   public Socket player_socket;
-  // public TurnList<T> turn_list;
   public Object obj;
 
   public PlayerThread(Socket player_socket) {
     this.player_socket = player_socket;
-    //this.turn_list = new TurnList<>("");
   }
 
   @Override
-  //@SuppressWarnings("unchecked")
   public void run(){
-    // handle TurnList receive from a player.
     try{
+        // Read Object from Clients
         InputStream o = player_socket.getInputStream();
         ObjectInputStream s = new ObjectInputStream(o);
         Object obj = s.readObject();
         this.obj = obj;
-
-        //this.turn_list = (TurnList<T>)obj;
     }catch(Exception e){
         e.printStackTrace();
     }
   }
-
-  // Send origin Map to all Players
-  /*
-  InputStream is = player_socket.getInputStream();
-  BufferedReader br = new BufferedReader(new InputStreamReader(is));
-  String msg = br.readLine();System.out.println("Received Message: "+msg);
-  */
-
 }
