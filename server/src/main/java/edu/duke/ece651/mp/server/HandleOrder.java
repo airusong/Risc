cdb5c26867c23a0af6a915a34c328fd62d00a299
yy295 <yy295@duke.edu>
2022-03-18 21:20:26 -0400

    addTempMap

--- server/src/main/java/edu/duke/ece651/mp/server/HandleOrder.java
+++ server/src/main/java/edu/duke/ece651/mp/server/HandleOrder.java
@@ -15,204 +15,253 @@ import edu.duke.ece651.mp.common.PathChecking;
 import edu.duke.ece651.mp.common.Territory;
 
 public class HandleOrder<T> {
-  public ArrayList<TurnList> all_order_list;
-  Map<T> theMap;
-  private final MoveChecking<T> moveChecker;
-  ArrayList<String> turnStatus;
+    public ArrayList<TurnList> all_order_list;
+    Map<T> theMap;
+    private final MoveChecking<T> moveChecker;
+    ArrayList<String> turnStatus;
 
-  HandleOrder() {
-    this.all_order_list = new ArrayList<TurnList>();
-    this.theMap = new V1Map<>();
-    this.moveChecker = null;
-    this.turnStatus = new ArrayList<>();
-  }
+    HandleOrder() {
+        this.all_order_list = new ArrayList<TurnList>();
+        this.theMap = new V1Map<>();
+        this.moveChecker = null;
+        this.turnStatus = new ArrayList<>();
+    }
 
-  HandleOrder(ArrayList<TurnList> all_order_list, Map<T> theMap, MoveChecking<T> moveChecker) {
-    this.all_order_list = all_order_list;
-    this.theMap = theMap;
-    this.moveChecker = moveChecker;
-    this.turnStatus = new ArrayList<>();
-  }
+    HandleOrder(ArrayList<TurnList> all_order_list, Map<T> theMap, MoveChecking<T> moveChecker) {
+        this.all_order_list = all_order_list;
+        this.theMap = theMap;
+        this.moveChecker = moveChecker;
+        this.turnStatus = new ArrayList<>();
+    }
 
-  /**
-   * Method to handle All Move Orders
-   * 
-   */
-  public void handleAllMoveOrder() {
-    for (int i = 0; i < all_order_list.size(); i++) {
-      TurnList curr = all_order_list.get(i);
-      for (int j = 0; j < curr.getListLength(); j++) {
-        Turn curr_turn = (Turn) (curr.order_list.get(j));
-        if (curr_turn.getTurnType().equals("Move")) {
-          handleSingleMoveOrder(curr_turn);
+    /**
+     * Method to handle All Move Orders
+     * 
+     */
+    public void handleAllMoveOrder() {
+        for (int i = 0; i < all_order_list.size(); i++) {
+            TurnList curr = all_order_list.get(i);
+            for (int j = 0; j < curr.getListLength(); j++) {
+                Turn curr_turn = (Turn) (curr.order_list.get(j));
+                if (curr_turn.getTurnType().equals("Move")) {
+                    handleSingleMoveOrder(curr_turn);
+                }
+            }
         }
-      }
     }
-  }
 
-  /**
-   * Method to handle All Attack Orders
-   * 
-   */
-  public void handleAllAttackOrder() {
-    // create a temporary map with correct attacker and defender unit number (requirement 5d)
-    for (int i = 0; i < all_order_list.size(); i++) {
-      TurnList curr = all_order_list.get(i);
-      for (int j = 0; j < curr.getListLength(); j++) {
-        Turn curr_turn = (Turn) curr.order_list.get(j);
-        if (curr_turn.getTurnType().equals("Attack")) {
-          handleSingleAttackOrder(curr_turn); // take an extra parameter for the temporary map
+    /**
+     * Method to handle All Attack Orders
+     * 
+     */
+    public void handleAllAttackOrder() {
+        // create a temporary map with correct attacker and defender unit number
+        // (requirement 5d)
+        Map<T> tempMap = theMap;
+        ArrayList<TurnList> attack_order_list;
+        TurnList attack_list;
+        ArrayList<TurnList> valid_attack_order_list = new ArrayList<TurnList>();
+
+        AttackChecking<T> ruleChecker = new AttackChecking<>();
+
+        // filter out valid attack orders from same player
+        for (int i = 0; i < all_order_list.size(); i++) {
+            TurnList curr = all_order_list.get(i);
+            TurnList curr_valid = new TurnList(curr.player_info);
+            for (int j = 0; j < curr.getListLength(); j++) {
+                Turn curr_turn = (Turn) curr.order_list.get(j);
+                if (curr_turn.getTurnType().equals("Attack") && ruleChecker.checkMyRule(theMap, curr_turn)) {
+                    curr_valid.order_list.add(curr_turn);
+                }
+            }
+            valid_attack_order_list.add(curr_valid);
         }
-      }
+        // update valid order lists
+        attack_order_list = valid_attack_order_list;
+
+
+        // // sort attack turns for the same player.
+        for (int i = 0; i < valid_attack_order_list.size(); i++) {
+            TurnList curr = valid_attack_order_list.get(i);
+            HashMap<String, ArrayList<Turn>> temp = new HashMap<String, ArrayList<Turn>>();
+            for (int j = 0; j < curr.getListLength(); j++) {
+                Turn curr_turn = (Turn) curr.order_list.get(j);
+                // Update the map
+
+
+                String des = curr_turn.getDestination();
+                if (temp.get(des) != null) {
+                    temp.get(des).add(curr_turn);
+                }
+                else{
+                    ArrayList<Turn> newTurnList = new ArrayList<Turn>();
+                    newTurnList.add(curr_turn);
+                    temp.put(des, newTurnList);
+                }
+                // handleSingleAttackOrder(curr_turn, tempMap); // take an extra parameter for
+                // the temporary map
+            }
+            for (ArrayList<Turn> t : temp.values()) {
+                handleSingleAttackOrder(t, tempMap);
+            }
+        }
     }
-  }
 
-  /**
-   * Method to handle Move Order
-   * 
-   */
-  public void handleSingleMoveOrder(Turn moveOrder) {
-    int unitsToMove = moveOrder.getNumber();
-    String dep = moveOrder.getSource();
-    String des = moveOrder.getDestination();
-    String player_color = moveOrder.getPlayerColor();
+    /**
+     * Method to handle Move Order
+     * 
+     */
+    public void handleSingleMoveOrder(Turn moveOrder) {
+        int unitsToMove = moveOrder.getNumber();
+        String dep = moveOrder.getSource();
+        String des = moveOrder.getDestination();
+        String player_color = moveOrder.getPlayerColor();
 
-    // Check Rules
-    String moveProblem = moveChecker.checkMoving(theMap, dep, des, unitsToMove, player_color);
+        // Check Rules
+        String moveProblem = moveChecker.checkMoving(theMap, dep, des, unitsToMove, player_color);
 
-    String moveResult = "";
+        String moveResult = "";
 
-    if (moveProblem == null) {
-      // update Territory & Map
-      int unit_num_dep = ((Territory<T>) theMap.getAllTerritories().get(dep)).getUnit();
-      int new_unit_num_dep = unit_num_dep - unitsToMove;
-      int unit_num_des = ((Territory<T>) theMap.getAllTerritories().get(des)).getUnit();
-      int new_unit_num_des = unit_num_des + unitsToMove;
-      theMap.updateMap(dep, des, new_unit_num_dep, new_unit_num_des);
-    }
-    else {
-      moveResult = "invalid (Reason: " + moveProblem + ")";
-    }
-
-   turnStatus.add(moveChecker.moveStatus + moveResult);
-  }
-
-  /**
-   * Method to handle Attack Order
-   * 
-   */
-  public void handleSingleAttackOrder(Turn attackOrder) {
-    // handle attack order
-    AttackChecking<T> ruleChecker = new AttackChecking<>();
-    String attackResult = "";
-    if (ruleChecker.checkMyRule(theMap, attackOrder)) { // rule is VALID
-      attackResult += resolveCombat(attackOrder);
-    }
-    turnStatus.add(ruleChecker.attackStatus + attackResult);
-  }
-
-  /**
-   * Method to resolve combat in any one territory
-   */
-  private String resolveCombat(Turn attackOrder) {
-    String combatResult;
-    int attacking_units = attackOrder.getNumber();
-    String attackerTerritory = attackOrder.getSource();
-    String defenderTerritory = attackOrder.getDestination();
-    String player_color = attackOrder.getPlayerColor();
-
-    Territory<T> attacker = theMap.getAllTerritories().get(attackerTerritory);
-    Territory<T> defender = theMap.getAllTerritories().get(defenderTerritory);
-    int defending_units = defender.getUnit();
-
-    Random attackerDice = new Random();
-    Random defenderDice = new Random();
-    int diceSides = 20;
-    System.out.println("Starting combat...");
-    while (true) { // start combat
-
-      // step-1: role a 20 sided twice for both players
-      int attackerDiceVal = attackerDice.nextInt(diceSides);
-      int defenderDiceVal = defenderDice.nextInt(diceSides);
-
-      // step-2: compare dice values. Lower loses 1 unit
-      String loserTerr;
-      int loserTerrRemainingUnits;
-      if (attackerDiceVal <= defenderDiceVal) { // attacker lost (in a tie, defender wins)
-        loserTerr = attackerTerritory;
-        attacking_units--;
-        loserTerrRemainingUnits = attacking_units;
-      }
-
-      else { // (attackerDiceVal > defenderDiceVal) - defender lost
-        loserTerr = defenderTerritory;
-        defending_units--;
-        loserTerrRemainingUnits = defending_units;
-      }
-
-      // step-3: check if the loser territory ran out of units
-      // if no, continue. If yes, update map with result
-      if (loserTerrRemainingUnits > 0) {
-        continue;
-      } else {
-        // attacker territory lost the units no matter what
-        theMap.updateTerritoryInMap(attackerTerritory, (attackOrder.getNumber()) * (-1)); // -1 for making it negative
-
-        int unitChange;
-        if (loserTerr == attackerTerritory) {
-          unitChange = defending_units - defender.getUnit();
-          theMap.updateTerritoryInMap(defenderTerritory, unitChange);
-          combatResult = "Defender won!";
+        if (moveProblem == null) {
+            // update Territory & Map
+            int unit_num_dep = ((Territory<T>) theMap.getAllTerritories().get(dep)).getUnit();
+            int new_unit_num_dep = unit_num_dep - unitsToMove;
+            int unit_num_des = ((Territory<T>) theMap.getAllTerritories().get(des)).getUnit();
+            int new_unit_num_des = unit_num_des + unitsToMove;
+            theMap.updateMap(dep, des, new_unit_num_dep, new_unit_num_des);
+        } else {
+            moveResult = "invalid (Reason: " + moveProblem + ")";
         }
 
-        else { // loserTerr == defenderTerritory
-          unitChange = attacking_units - defender.getUnit();
-          theMap.updateTerritoryInMap(defenderTerritory, unitChange, player_color);
-          combatResult = "Attacker won!";
-        }
-        break;
-      }
+        turnStatus.add(moveChecker.moveStatus + moveResult);
     }
-    return combatResult;
 
-  }
+    /**
+     * Method to handle Attack Order
+     * 
+     */
+    public void handleSingleAttackOrder(ArrayList<Turn> attackOrder, Map<T> tempMap) {
+        // handle attack order
+        AttackChecking<T> ruleChecker = new AttackChecking<>();
+        String attackResult = "";
+        if (ruleChecker.checkMyRule(theMap, attackOrder) != null) { // rule is VALID
+            attackResult += resolveCombat(attackOrder,tempMap);
+        }
+        turnStatus.add(ruleChecker.attackStatus + attackResult);
+    }
 
-  /**
-   * Method to decrease one unit from each territory Used after each turn
-   * 
-   */
-  public void updateMapbyOneUnit() {
-    theMap.updateMapbyOneUnit();
-  }
+    /**
+     * Method to resolve combat in any one territory
+     */
+    private String resolveCombat(ArrayList<Turn> attackOrder, Map<T> tempMap) {
+        String combatResult;
+        int attacking_units = 0;
+        String attackerTerritory = "";
+        String defenderTerritory = "";
+        String player_color = "";
 
-  /**
-   * Method to handle All kinds of Orders
-   * 
-   */
-  public Map<T> handleOrders(ArrayList<TurnList> all_order_list, Map<T> theMap) {
-    this.all_order_list = all_order_list;
-    this.theMap = theMap;
-    handleAllMoveOrder();
-    handleAllAttackOrder();
+        for(Turn t: attackOrder){
+            attacking_units += t.getNumber(); // add up units
+            attackerTerritory = t.getSource(); // any Source
+            defenderTerritory = t.getDestination(); // same Destination
+            player_color = t.getPlayerColor();
+            //theMap.updateTerritoryInMap(attackerTerritory, (t.getNumber()) * (-1)); // reduce #units in all attackerTerritory
+        }
 
-    // updateMapbyOneUnit(theMap);
+        Territory<T> attacker = tempMap.getAllTerritories().get(attackerTerritory);
+        Territory<T> defender = tempMap.getAllTerritories().get(defenderTerritory);
+        int defending_units = defender.getUnit();
 
-    // NEED TO RETURN UPDATED MAP
-    return theMap;
-  }
+        Random attackerDice = new Random();
+        Random defenderDice = new Random();
+        int diceSides = 20;
+        System.out.println("Starting combat...");
+        while (true) { // start combat
 
-  
-  /**
-   * Method to handle All kinds of Orders
-   * 
-   */
-  /*
-  public String handleOrders() {
-    String moveProblem = this.handleAllMoveOrder();
-    handleAllAttackOrder();
-    updateMapbyOneUnit();
-    return moveProblem;
-  }
-  */
+            // step-1: role a 20 sided twice for both players
+            int attackerDiceVal = attackerDice.nextInt(diceSides);
+            int defenderDiceVal = defenderDice.nextInt(diceSides);
+
+            // step-2: compare dice values. Lower loses 1 unit
+            String loserTerr;
+            int loserTerrRemainingUnits;
+            if (attackerDiceVal <= defenderDiceVal) { // attacker lost (in a tie, defender wins)
+                loserTerr = attackerTerritory;
+                attacking_units--;
+                loserTerrRemainingUnits = attacking_units;
+            }
+
+            else { // (attackerDiceVal > defenderDiceVal) - defender lost
+                loserTerr = defenderTerritory;
+                defending_units--;
+                loserTerrRemainingUnits = defending_units;
+            }
+
+            // step-3: check if the loser territory ran out of units
+            // if no, continue. If yes, update map with result
+            if (loserTerrRemainingUnits > 0) {
+                continue;
+            } else {
+                // attacker territory lost the units no matter what
+                // theMap.updateTerritoryInMap(attackerTerritory, (attackOrder.getNumber()) * (-1)); // -1 for making it
+                                                                                                  // negative
+
+                int unitChange;
+                if (loserTerr == attackerTerritory) {
+                    unitChange = defending_units - defender.getUnit();
+                    tempMap.updateTerritoryInMap(defenderTerritory, unitChange);
+                    combatResult = "Defender won!";
+                }
+
+                else { // loserTerr == defenderTerritory
+                    unitChange = attacking_units - defender.getUnit();
+                    tempMap.updateTerritoryInMap(defenderTerritory, unitChange, player_color);
+                    combatResult = "Attacker won!";
+                }
+                break;
+            }
+        }
+        // Update True Map
+        this.theMap = tempMap;
+        return combatResult;
+
+    }
+
+    /**
+     * Method to decrease one unit from each territory Used after each turn
+     * 
+     */
+    public void updateMapbyOneUnit() {
+        theMap.updateMapbyOneUnit();
+    }
+
+    /**
+     * Method to handle All kinds of Orders
+     * 
+     */
+    public Map<T> handleOrders(ArrayList<TurnList> all_order_list, Map<T> theMap) {
+        this.all_order_list = all_order_list;
+        this.theMap = theMap;
+        handleAllMoveOrder();
+        handleAllAttackOrder();
+
+        // updateMapbyOneUnit(theMap);
+
+        // NEED TO RETURN UPDATED MAP
+        return theMap;
+    }
+
+    /**
+     * Method to handle All kinds of Orders
+     * 
+     */
+    /*
+     * public String handleOrders() {
+     * String moveProblem = this.handleAllMoveOrder();
+     * handleAllAttackOrder();
+     * updateMapbyOneUnit();
+     * return moveProblem;
+     * }
+     */
 
 }
