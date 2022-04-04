0ddcc63b493755d5f426a566936f00c03f837b62
as1134 <airu.song@duke.edu>
2022-04-03 20:46:19 -0400

    add action option

--- client/src/main/java/edu/duke/ece651/mp/client/GameController.java
+++ client/src/main/java/edu/duke/ece651/mp/client/GameController.java
@@ -1,16 +1,40 @@
 package edu.duke.ece651.mp.client;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 
-public class GameController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class GameController implements Initializable {
+    @FXML
+    public ChoiceBox<String> playeraction=new ChoiceBox<>();
+    @FXML
+    public MenuButton from;
+    @FXML
+    public MenuButton to;
+    @FXML
+    public Button commit;
+    @FXML
+    public TextField unit;
+    @FXML
+    public Button order;
     @FXML
     private TextField player_info;
 
     public void setName(String name){
         player_info.setText(name);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        playeraction.getItems().add("move");
+        playeraction.getItems().add("attack");
+        playeraction.getItems().add("Upgrade");
     }
 
 }
