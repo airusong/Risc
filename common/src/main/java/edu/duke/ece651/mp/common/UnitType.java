dc91c3a1809b44924ce62d0c0ba6cb8451c14247
YUE <yue@YUEdeMacBook-Pro.local>
2022-04-06 01:07:11 -0400

    change-clover-setting-for-enum

--- common/src/main/java/edu/duke/ece651/mp/common/UnitType.java
+++ common/src/main/java/edu/duke/ece651/mp/common/UnitType.java
@@ -1,5 +1,6 @@
 package edu.duke.ece651.mp.common;
 
 public enum UnitType{
-    ALEVEL, BLEVEL, CLEVEL, DLEVEL, ELEVEL, FLEVEL, GLEVEL, HLEVEL
+    ALEVEL, BLEVEL, CLEVEL, DLEVEL, ELEVEL, FLEVEL, GLEVEL, HLEVEL;
+
 }
